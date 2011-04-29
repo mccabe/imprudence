ae5d5053cd20ce5148715b786931a0aa7625fe82
Patrick Sapinski <patrick@sapinski.com>
2011-04-28 21:54:25 -0400

    updated license on kowopenregionsettings to LGPL

--- linden/indra/newview/kowopenregionsettings.cpp
+++ linden/indra/newview/kowopenregionsettings.cpp
@@ -1,28 +1,24 @@
-/*
+/* $LicenseInfo:firstyear=2011&license=viewerlgpl$
+ * Astra Viewer Source Code
  * @file kowopenregionsettings.cpp
  * @brief Handler for OpenRegionInfo event queue message.
+ * Copyright (C) 2011, Patrick Sapinski
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation;
+ * version 2.1 of the License only.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
- * Copyright (c) 2010, Patrick Sapinski
- *
- * The source code in this file ("Source Code") is provided to you
- * under the terms of the GNU General Public License, version 2.0
- * ("GPL"). Terms of the GPL can be found in doc/GPL-license.txt in
- * this distribution, or online at
- * http://secondlifegrid.net/programs/open_source/licensing/gplv2
- *
- * There are special exceptions to the terms and conditions of the GPL as
- * it is applied to this Source Code. View the full text of the exception
- * in the file doc/FLOSS-exception.txt in this software distribution, or
- * online at
- * http://secondlifegrid.net/programs/open_source/licensing/flossexception
- *
- * By copying, modifying or distributing this software, you acknowledge
- * that you have read and understood your obligations described above,
- * and agree to abide by those obligations.
- *
- * ALL SOURCE CODE IS PROVIDED "AS IS." THE AUTHOR MAKES NO
- * WARRANTIES, EXPRESS, IMPLIED OR OTHERWISE, REGARDING ITS ACCURACY,
- * COMPLETENESS OR PERFORMANCE.
+ * $/LicenseInfo$
  */
 
 #include "llviewerprecompiledheaders.h"
